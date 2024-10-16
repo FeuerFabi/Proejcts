00a7a8f60dc8698973146abfef10bb5c372ff4b8
FeuerFabi <fabian.sult06@gmail.com>
2024-10-16 15:55:33 +0200

    Update Login.java
    added constant string array for better code overview

--- aTM/src/Login.java
+++ aTM/src/Login.java
@@ -1,28 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Login extends Konto
 {
+    private final String[] displayText = {
+            "email: ",  // 0
+            "email (Keine Whitespaces und muss '@' haben): ", // 1
+            "Falsche Email!", // 2
+            "passwort: ", // 3
+            "Falsches Passwort!", // 4
+            "Bitte registrieren!", // 5
+            "Bitte anmelden!", // 6
+            "Du bist nun angemeldet!" // 7
+    };
+
     private String email;
     private String password;
     private String checkEmail;
     private String checkPassword;
 
     Login() {
-
+        super();
     }
 
     Scanner scanner = new Scanner(System.in);
 
-    public Login(String email, String password) {
-        super(email, password);
-    }
-
     public void registrierung() {
+        System.out.println(displayText[5]);
         do {
-        System.out.println("email (Keine Whitespaces und muss '@' haben): ");
+        System.out.println(displayText[1]);
         email = scanner.nextLine();
         } while (email.isEmpty() || !email.contains("@"));
-        System.out.println("password: ");
+        System.out.println(displayText[3]);
         password = scanner.nextLine();
     }
 
@@ -35,29 +44,51 @@ public class Login extends Konto
     }
 
     public void anmelden() {
-        System.out.println("Anmelden!");
-        System.out.println("email: ");
+        System.out.println(displayText[6]);
+        System.out.println(displayText[0]);
         checkEmail = scanner.nextLine();
         boolean check = checkEmail(checkEmail);
 
         while (check == false) {
-            System.out.println("Falsche Email!");
-            System.out.println("email: ");
+            System.out.println(displayText[2]);
+            System.out.println(displayText[0]);
             checkEmail = scanner.nextLine();
             check = checkEmail(checkEmail);
         }
 
-        System.out.println("password: ");
+        System.out.println(displayText[3]);
         checkPassword = scanner.nextLine();
         boolean checkPass = checkPass(checkPassword);
 
         while (checkPass == false) {
-            System.out.println("Falsches Passwort!");
-            System.out.println("passwort: ");
+            System.out.println(displayText[4]);
+            System.out.println(displayText[3]);
             checkPassword = scanner.nextLine();
             checkPass = checkPass(checkPassword);
         }
 
-        System.out.println("Du bist nun angemeldet!");
+        System.out.println(displayText[7]);
     }
 }
+
+/*
+ System.out.println("Bitte anmelden!");
+
+        while (checkMail == false) {
+            System.out.println("Falsche Email!");
+            System.out.println("email: ");
+            String ccEmail = scanner.nextLine();
+            checkMail = checkLogin.checkEmail(ccEmail);
+        }
+
+        System.out.println("password: ");
+        String cPassword = scanner.nextLine();
+        boolean checkPassword = checkLogin.checkPass(cPassword);
+
+        while (checkPassword == false) {
+            System.out.println("Falsches Passwort!");
+            System.out.println("password: ");
+            String ccPassword = scanner.nextLine();
+            checkPassword = checkLogin.checkPass(ccPassword);
+        }
+ */
