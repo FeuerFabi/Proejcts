42f255fc3367315e2c86a7a0cfa1c97ebd45e685
FeuerFabi <fabian.sult06@gmail.com>
2024-10-16 08:38:15 +0200

    added whitespaces for better view Main.java

--- CoinFlip/src/Main.java
+++ CoinFlip/src/Main.java
@@ -15,25 +15,33 @@ public class Main {
                 Finde es jetzt heraus indem du Coinflip spielst!
                 (min einzahlung 50c max 4e)
                 """);
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Wähle nun deinen Einsatz -> 50c, 1e, 2e, 4e");
         String user_input = input.nextLine();
+        
         if (!user_input.equals("50") && !user_input.equals("1") && !user_input.equals("2") && !user_input.equals("4")) {
             System.err.println("Fehler beim lesen des Einsatz!");
             System.exit(1);
         }
+        
         einsatz = Integer.parseInt(user_input);
+        
         do {
-
+            
             System.out.printf("Dein Einsatz von %d kann nun verdoppelt werden! ", einsatz);
             System.out.println("Kopf oder Zahl was wird es nun werden! Entscheide dich JETZT für eine Seite -> Kopf ODER Zahl");
             String head_or_num = input.nextLine();
+            
             if (random() > 0.5) {
                 if (head_or_num.equals("Kopf")) {
+                    
                     System.out.println("Glückwunsch! Es ist Kopf! dein einsatz hat sich nun verdoppelt!");
                     System.out.printf("Dein neues Guthaben beträgt %d! ", einsatz *= 2);
                     System.out.println("Möchtest du weiter dein Glück versuchen oder Auszahlen -> w (weiter), a (auszahlen)");
+                    
                     String input_cont = input.nextLine();
+                    
                     if (input_cont.equals("w")) {
                         cont = true;
                     }
@@ -41,20 +49,29 @@ public class Main {
                         cont = false;
                     }
                 } else if (head_or_num.equals("Zahl")) {
+                    
                     System.out.printf("Schade! Es ist Kopf! viel Erfolg beim nächsten mal! dein neues Guthaben beträgt %d!", einsatz = 0);
                     System.exit(1);
+                    
                 } else {
+                    
                     System.err.println("Fehler beim lesen des Spielwertes!");
                     System.exit(1);
+                    
                 }
             } else {
                 if (head_or_num.equals("Kopf")) {
+                    
                     System.out.printf("Schade! Es ist Zahl! viel Erfolg beim nächsten mal! dein neues Guthaben beträgt %d!", einsatz = 0);
+                    
                 } else if (head_or_num.equals("Zahl")) {
+                    
                     System.out.println("Glückwunsch! Es ist zahl! dein einsatz hat sich nun verdoppelt!");
                     System.out.printf("Dein neues Guthaben beträgt %d! ", einsatz *= 2);
                     System.out.println("Möchtest du weiter dein Glück versuchen oder Auszahlen -> w (weiter), a (auszahlen)");
+                    
                     String input_cont = input.nextLine();
+                    
                     if (input_cont.equals("w")) {
                         cont = true;
                     }
@@ -65,7 +82,9 @@ public class Main {
             }
         } while (cont == true);
         if (einsatz != 0) {
+            
             System.out.printf("Dein GEWINN von %d wird nun ausgezahlt! Bis zur nächsten Partie Coinflip!", einsatz);
+            
         }
         else {
             System.exit(1);
