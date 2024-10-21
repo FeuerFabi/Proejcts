74f37eb669cf571b5ac5ac06adda3e1107ce1cd3
FeuerFabi <fabian.sult06@gmail.com>
2024-10-21 14:57:12 +0200

    Create KontoInterface.java

---
+++ aTM/src/KontoInterface.java
@@ -0,0 +1,14 @@
+public interface KontoInterface {
+    String[] displayText = {
+            "Nicht genug geld auf dem Konto! du hast %d EURO und hast versucht %d EURO abzubuchen!", // 0
+            "Drück 1 zum auszahlen, 2 zum einzahlen, 3 um Kontostand anzuzeigen, 4 zum abbrechen", // 1
+            "Drücke 1 für 10 EURO, 2 für 20 EURO, 3 für 50 EURO, 4 für anderer Betrag", // 2
+            "Bitte gewünschten Betrag eingeben: ", // 3
+            "Bitte eine Zahl von 1 - 4 eingeben!", // 4
+            "Du hast erfolgreich %d EURO abgehoben!", // 5
+            "Bitte Betrag eingeben! ", // 6
+            "Folgender Betrag wird auf dein konto gebucht %d EURO!", // 7
+            "Dein Kontostand beträgt %d EURO!", // 8
+            "Fehler! Bitte eine 1, 2, 4 oder 4 eingeben" //9
+    };
+}
