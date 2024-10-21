dfca852dd16801c09c9222cfeda7eadeacae809f
FeuerFabi <fabian.sult06@gmail.com>
2024-10-21 11:45:54 +0200

    Update LoginInterface.java

--- aTM/src/LoginInterface.java
+++ aTM/src/LoginInterface.java
@@ -1,4 +1,16 @@
 interface LoginInterface {
+
+    String[] displayText = {
+            "email: ",  // 0
+            "email (Keine Whitespaces und muss '@' haben): ", // 1
+            "Falsche Email!", // 2
+            "passwort: ", // 3
+            "Falsches Passwort!", // 4
+            "Bitte registrieren!", // 5
+            "Bitte anmelden!", // 6
+            "Du bist nun angemeldet!" // 7
+    };
+
     void registrierung();
-     void anmelden();
+    void anmelden();
 }
