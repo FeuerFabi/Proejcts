9cefce82b04cf79c445b30c235e0e3db2ada641a
FeuerFabi <fabian.sult06@gmail.com>
2024-10-21 11:44:39 +0200

    Update Login.java

--- aTM/src/Login.java
+++ aTM/src/Login.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class Login extends Konto implements LoginInterface {
 
-    private final String[] displayText = {
-            "email: ",  // 0
-            "email (Keine Whitespaces und muss '@' haben): ", // 1
-            "Falsche Email!", // 2
-            "passwort: ", // 3
-            "Falsches Passwort!", // 4
-            "Bitte registrieren!", // 5
-            "Bitte anmelden!", // 6
-            "Du bist nun angemeldet!" // 7
-    };
-
     private String email, password;
 
     Login() {
@@ -22,19 +11,16 @@ public class Login extends Konto implements LoginInterface {
     Scanner scanner = new Scanner(System.in);
 
     @Override public void registrierung() {
-        System.out.println(displayText[5]);
-        do {
-        System.out.println(displayText[1]);
-        email = scanner.nextLine();
-        } while (email.isEmpty() || !email.contains("@"));
-        System.out.println(displayText[3]);
-        password = scanner.nextLine();
+        Registrierung register = new Registrierung(LoginInterface.displayText);
+        register.registrieren();
+        this.email = register.email;
+        this.password = register.password;
     }
 
 
 
     @Override public void anmelden() {
-        CheckLogin checkLogin = new CheckLogin(this.displayText, this.email, this.password);
+        CheckLogin checkLogin = new CheckLogin(LoginInterface.displayText, this.email, this.password);
         checkLogin.checkLogin();
     }
 }
