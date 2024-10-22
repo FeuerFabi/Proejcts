bf18566f056d2af366264372e11cb10f3ef13625
FeuerFabi <fabian.sult06@gmail.com>
2024-10-22 15:37:27 +0200

    Create GameLoop Object, restructructured code for better view and improved game logic

--- CoinFlip/src/Main.java
+++ CoinFlip/src/Main.java
@@ -1,14 +1,9 @@
-import java.util.Scanner;
-
-import static java.lang.Math.random;
-
 /*
     Author: Fabi
  */
 public class Main {
     public static void main(String[] args) {
-        CoinGame coin = new CoinGame();
-        coin.einsatz();
-        coin.gameLoop();
+        GameLoop loop = new GameLoop();
+        loop.gameLoop();
     }
 }
